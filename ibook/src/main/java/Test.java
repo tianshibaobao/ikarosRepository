7789436d99abeaa99c14f00116eed6319ef96706
tianshibaobao <56427470+tianshibaobao@users.noreply.github.com>
2019-10-11 15:14:30 +0800

    Add files via upload

---
+++ ibook/src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author ：ikaros
+ * @date ：Created in 2019/10/11 14:30
+ * @description：
+ * @email:478308676@qq.com
+ * @modified By：
+ * @version: $
+ */
+public class Test {
+    public static void  main(String[] args){
+        System.out.println("helloworld");
+    }
+}
